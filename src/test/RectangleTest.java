26bb06c3d416e23b153e4a1f230b863086f8bfe5
SinaBolouki <sina.bolouki@gmail.com>
2021-10-29 13:03:13 +0330

    added setter tests

--- src/test/RectangleTest.java
+++ src/test/RectangleTest.java
@@ -9,4 +9,18 @@ public class RectangleTest {
         Rectangle rectangle = new Rectangle(10, 30);
         Assert.assertEquals(rectangle.getArea(), 300, 0.001);
     }
+
+    @Test
+    public void changeHeightTest() {
+        Rectangle rectangle = new Rectangle(10, 30);
+        rectangle.setHeight(20);
+        Assert.assertEquals(rectangle.getArea(), 600, 0.001);
+    }
+
+    @Test
+    public void changeWidthTest() {
+        Rectangle rectangle = new Rectangle(10, 30);
+        rectangle.setWidth(40);
+        Assert.assertEquals(rectangle.getArea(), 400, 0.001);
+    }
 }
