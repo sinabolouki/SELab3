2df372fe5b909f42d82f831d84132c4cd4cc00b5
SinaBolouki <sina.bolouki@gmail.com>
2021-10-29 12:44:12 +0330

    added missing methods

--- src/main/Rectangle.java
+++ src/main/Rectangle.java
@@ -1,5 +1,15 @@
 package main;
 
 public class Rectangle {
+    private double height;
+    private double width;
 
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getArea() {
+        return height * width;
+    }
 }
