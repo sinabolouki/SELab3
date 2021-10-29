536e5db8251a53d9d3385c6ac2b5050f23b08849
SinaBolouki <sina.bolouki@gmail.com>
2021-10-29 12:44:47 +0330

    added delta to assertEquals

--- src/test/RectangleTest.java
+++ src/test/RectangleTest.java
@@ -7,6 +7,6 @@ public class RectangleTest {
     @Test
     public void calculateAreaTest() {
         Rectangle rectangle = new Rectangle(10, 30);
-        Assert.assertEquals(rectangle.getArea(), 300);
+        Assert.assertEquals(rectangle.getArea(), 300, 0.001);
     }
 }
